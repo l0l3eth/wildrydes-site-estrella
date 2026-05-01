0730afcfb236730e394176b09b216784c65929fd
lolbeth <117290222+l0l3eth@users.noreply.github.com>
2026-05-01 12:14:55 -0600

    Invoke URL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1'
     },
     api: {
-        invokeUrl: ''
+        invokeUrl: 'https://rrnxgj87mf.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
